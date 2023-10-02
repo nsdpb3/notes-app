e84952f813ef7acf26787fd0d2a4b8a4f8d2d2c6
nsdbp3 <xramov14881@gmail.com>
2023-10-02 23:22:23 +0400

    Вставил компонент Message

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
 	<div class="wrapper">
-		<div class="wrapper-content"></div>
+		<div class="wrapper-content">
+			<section>
+				<div class="container">
+					<h1>{{ title }}</h1>
+					<message />
+				</div>
+			</section>
+		</div>
 	</div>
 </template>
 
@@ -11,7 +18,9 @@ export default {
 		Message,
 	},
 	data() {
-		return {};
+		return {
+			title: "Notes App",
+		};
 	},
 };
 </script>
--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,4 +1,6 @@
-<template></template>
+<template>
+	<h2>Сообщение</h2>
+</template>
 
 <script>
 export default {};
