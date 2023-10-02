3c3f5827733faef0b5dd74f3d5e646016f64155b
nsdbp3 <xramov14881@gmail.com>
2023-10-02 23:07:54 +0400

    initial

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="wrapper">
+		<div class="wrapper-content"></div>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	},
+};
+</script>
+
+<style></style>
